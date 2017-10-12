1f6d68c5b8ab8e84f06bd8674bda7f0582d5be90
Andrew Stilwell <stilwea1@my.erau.edu>
2017-10-12 15:05:45 -0400

    Added TODO statements.
    Signed-off-by: Andrew Stilwell <stilwea1@my.erau.edu>

--- Sandbox/InputConversion.py
+++ Sandbox/InputConversion.py
@@ -1,6 +1,26 @@
+import random
+import sys
+
+
+def printf(format, *args):
+    sys.stdout.write(format % args)
+
 # TODO: Get current GPS, speed, and heading for car
+ORIGIN = [0, 0]
+
+cur_pos = [0, 0]
+
+for i in range(0, 10):
+    prev_pos = cur_pos
+    cur_pos = [random.randint(0, 10000), random.randint(0, 10000)]
+
+    print prev_pos, cur_pos
 
 # TODO: Get output vector from simulation
+    sim_pos = [random.randint(0, 10000), random.randint(0, 10000)]
+
+    print sim_pos
+    print
 
 # TODO: Calculate current location in reference to new target location
 
