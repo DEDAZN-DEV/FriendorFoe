6ce2f57cb6cff59d2b4e799986e9cd8c390660da
Andrew Stilwell <stilwea1@my.erau.edu>
2017-10-12 14:33:15 -0400

    Added TODO statements.
    Signed-off-by: Andrew Stilwell <stilwea1@my.erau.edu>

--- Sandbox/InputConversion.py
+++ Sandbox/InputConversion.py
@@ -1,1 +1,13 @@
-# Get current GPS and speed for car
+# TODO: Get current GPS, speed, and heading for car
+
+# TODO: Get output vector from simulation
+
+# TODO: Calculate current location in reference to new target location
+
+# TODO: Determine magnitude and speed vector
+
+# TODO: Determine what signals are needed to direct car SERVO
+
+# TODO: Determine what signals are needed to direct car ESC
+
+# TODO: Transmit signals to car through WiFi
