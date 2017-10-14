47eac5775351148e719cb1f9e144d546b96e0263
Andrew Stilwell <stilwea1@my.erau.edu>
2017-10-14 13:35:41 -0400

    Fix commit.
    Signed-off-by: Andrew Stilwell <stilwea1@my.erau.edu>

--- Sandbox/InputConversion.py
+++ Sandbox/InputConversion.py
@@ -1,24 +1,32 @@
 import random
 import sys
 
+import matplotlib.pyplot as plt
+
 
 def printf(format, *args):
     sys.stdout.write(format % args)
 
-
 # TODO: Get current GPS, speed, and heading for car
 ORIGIN = [0, 0]
+xPosStorage = []
+yPosStorage = []
 
 init_pos = ORIGIN
+
+# while(True):
 cur_pos = init_pos
 
 for i in range(0, 10):
-    prev_pos = cur_pos
-    cur_pos = [cur_pos[0] + random.randint(0, 10), cur_pos[1] + random.randint(0, 10)]
+    xPosStorage.append(cur_pos[0])
+    yPosStorage.append(cur_pos[1])
+    cur_pos = [cur_pos[0] + random.randint(-5, 5), cur_pos[1] + random.randint(-5, 5)]
 
-    print(prev_pos, cur_pos)
-
-    # Testing...1...2...3. Testing.
+# Testing...1...2...3. Testing. This is a test.
+plt.plot(xPosStorage, yPosStorage)
+for xy in zip(xPosStorage, yPosStorage):
+    plt.annotate('(%s, %s)' % xy, xy=xy, textcoords='data')
+plt.show()
 
 # TODO: Get output vector from simulation
 
@@ -31,3 +39,5 @@ for i in range(0, 10):
 # TODO: Determine what signals are needed to direct car ESC
 
 # TODO: Transmit signals to car through WiFi
+
+# 12 turn, max power 40.24 watts @ 7772 RPM
