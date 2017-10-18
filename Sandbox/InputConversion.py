2d21b5ba3c8e959ee7d24b20e14db8a010808da6
Andrew Stilwell <stilwea1@my.erau.edu>
2017-10-18 14:50:53 -0400

    Modified some variables and reorganized code...still need to implement them as functions instead of procedurally executed code.
    Signed-off-by: Andrew Stilwell <dedicated.asian@gmail.com>

--- Sandbox/InputConversion.py
+++ Sandbox/InputConversion.py
@@ -9,21 +9,22 @@ def printf(format, *args):
 
 
 # TODO: Get current GPS, speed, and heading for car
-# STATIC DECLARATIONS
-ORIGIN = [0, 0]
-ACCELERATION = 10  # m/s
-UPDATE_INTERVAL = 0.003  # 0.003 s between vector update
-TEST_ITERATIONS = 25
-POSMAPBUFFERSIZE = 2
+# ADJUSTABLE VARIABLES
+A_ORIGIN = [0, 0]
+A_ACCELERATION = 10  # m/s
+A_UPDATE_INTERVAL = 0.003  # 3ms between vector update
+A_TEST_ITERATIONS = 25
+A_POSMAPBUFFERSIZE = 250
+A_DIRCHANGEFACTOR = .15  # % chance of changing direction
 
-# Initialization of variables
-counter = 0
+# FLAGS
+F_INIT = True
 
 # STORAGE LISTS
 xPosStorage = []
 yPosStorage = []
 
-cur_pos = ORIGIN
+cur_pos = A_ORIGIN
 
 # Initial setup for mapping
 plt.axhline(0, color='red')
@@ -36,28 +37,41 @@ while True:
     yPosStorage.append(cur_pos[1])
 
     # Remove oldest data
-    if len(xPosStorage) > POSMAPBUFFERSIZE:
+    if len(xPosStorage) > A_POSMAPBUFFERSIZE:
         xPosStorage.pop(0)
         yPosStorage.pop(0)
 
-    print(xPosStorage)
+    # print(xPosStorage)
 
     # Map plot
-    plt.plot(xPosStorage, yPosStorage, ':')
-    # plt.annotate(counter, (xPosStorage[counter], yPosStorage[counter]))  # label section
-    counter = counter + 1
+    plt.plot(xPosStorage, yPosStorage, color='black', linewidth=1.25)
+    plt.axis([0.0, 64.0, 0.0, 100.0])
+    # plt.axis([0.0, 16.0, 0.0, 25.0])  # test values for axis
+    plt.autoscale(False)
     plt.pause(0.003)
+    plt.clf()
+    plt.draw()
 
     # TODO: Get output vector from simulation
 
     # Simulate output vector from the simulator
-    newVector = [random.uniform(-13.4, 13.4), random.uniform(-13.4, 13.4)]
+    if random.uniform(0, 1) < A_DIRCHANGEFACTOR or F_INIT is True:
+        newVector = [random.uniform(-13.4, 13.4), random.uniform(-13.4, 13.4)]
+        F_INIT = False
 
     # Calculate new x and y coordinate based on last position
-    xDelta = (newVector[0] * UPDATE_INTERVAL) + ((1 / 2) * ACCELERATION * (UPDATE_INTERVAL ** 2))
-    yDelta = (newVector[1] * UPDATE_INTERVAL) + ((1 / 2) * ACCELERATION * (UPDATE_INTERVAL ** 2))
+    xDelta = (newVector[0] * A_UPDATE_INTERVAL) + ((1 / 2) * A_ACCELERATION * (A_UPDATE_INTERVAL ** 2))
+    yDelta = (newVector[1] * A_UPDATE_INTERVAL) + ((1 / 2) * A_ACCELERATION * (A_UPDATE_INTERVAL ** 2))
 
+    temp_pos = cur_pos
     cur_pos = [cur_pos[0] + xDelta, cur_pos[1] + yDelta]
+
+    while cur_pos[0] < 0.0 or cur_pos[1] < 0.0:
+        cur_pos = temp_pos
+        newVector = [random.uniform(-13.4, 13.4), random.uniform(-13.4, 13.4)]
+        xDelta = (newVector[0] * A_UPDATE_INTERVAL) + ((1 / 2) * A_ACCELERATION * (A_UPDATE_INTERVAL ** 2))
+        yDelta = (newVector[1] * A_UPDATE_INTERVAL) + ((1 / 2) * A_ACCELERATION * (A_UPDATE_INTERVAL ** 2))
+        cur_pos = [cur_pos[0] + xDelta, cur_pos[1] + yDelta]
 
     # print(cur_pos)
 
