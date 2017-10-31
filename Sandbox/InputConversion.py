a147e40dc53a61876d7ef04701ac701a6cfb8988
Andrew Stilwell <stilwea1@my.erau.edu>
2017-10-31 14:23:24 -0400

    Moved server settings to global declarations, added documentation stubs, and renamed variables to conform with Python coding standards.
    Signed-off-by: AStilwell <stilwea1@my.erau.edu>

--- Sandbox/InputConversion.py
+++ Sandbox/InputConversion.py
@@ -10,7 +10,6 @@ import time
 from datetime import datetime
 
 # TODO: Get current GPS, speed, and heading for car
-
 A_ORIGIN = [0, 0]  # ADJUSTABLE VARIABLES (GLOBAL)
 A_ACCELERATION = 10  # m/s
 A_UPDATE_INTERVAL = 0.5  # 2Hz refresh rate
@@ -18,153 +17,213 @@ A_TEST_ITERATIONS = 25
 A_POSMAPBUFFERSIZE = 250
 A_DIRCHANGEFACTOR = 0.25  # % chance of changing velocity input
 A_MAXVELOCITY = 13.4  # m/s
+A_SERVER_IP = "192.168.0.103"
+A_SERVER_PORT = 7777
 
 
 def main():
-    A = threading.Thread(target=run, args=("Drone A",))
-    A.start()
-    # B = threading.Thread(target=run, args=("Drone B",))
-    # B.start()
-    # C = threading.Thread(target=run, args=("Drone C",))
-    # C.start()
+    """
+
+    @return:
+    """
+
+    a = threading.Thread(target=run, args=("Drone A",))
+    a.start()
+    # b = threading.Thread(target=run, args=("Drone B",))
+    # b.start()
+    # c = threading.Thread(target=run, args=("Drone C",))
+    # c.start()
 
 
-def run(droneName):
-    F_INIT = True  # FLAGS
+def run(dronename):
+    """
 
-    xPosStorage = []  # STORAGE LISTS
-    yPosStorage = []
+    @param dronename:
+    @return:
+    """
+
+    f_init = True  # FLAGS
+
+    xpossstorage = []  # STORAGE LISTS
+    yposstorage = []
 
     counter = 0  # LOCAL VARIABLES
-    carData = [0.0, 0.0, 0.0, 0.0]
+    cardata = [0.0, 0.0, 0.0, 0.0]
 
     while True:
-        xPosStorage.append(carData[0])
-        yPosStorage.append(carData[1])
+        xpossstorage.append(cardata[0])
+        yposstorage.append(cardata[1])
 
-        if len(xPosStorage) > A_POSMAPBUFFERSIZE:  # Remove oldest data from buffer
-            xPosStorage.pop(0)
-            yPosStorage.pop(0)
+        if len(xpossstorage) > A_POSMAPBUFFERSIZE:  # Remove oldest data from buffer
+            xpossstorage.pop(0)
+            yposstorage.pop(0)
 
         # TODO: Get output vector from simulation
-        temp_data = carData[:]
+        temp_data = cardata[:]
 
-        if random.uniform(0, 1) < A_DIRCHANGEFACTOR or F_INIT is True:  # Simulate output vector from the simulator
-            vector = genRandomVector()
-            F_INIT = False
-            carData = updatePos(vector, True, temp_data)
+        if random.uniform(0, 1) < A_DIRCHANGEFACTOR or f_init is True:  # Simulate output vector from the simulator
+            vector = genrandomvector()
+            f_init = False
+            cardata = updatepos(vector, True, temp_data)
         else:
-            carData = updatePos(vector, False, temp_data)
+            cardata = updatepos(vector, False, temp_data)
 
-        while carData[0] < 0.0 or carData[1] < 0.0 or carData[0] > 100.0 or carData[1] > 64.0:
-            print(bcolors.WARNING + str(
-                datetime.now()) + " [WARNING] " + droneName + ": Current heading will hit or exceed boundary edge! Recalculating..." + bcolors.ENDC)
-            vector = genRandomVector()
-            carData = updatePos(vector, True, temp_data)
+        while cardata[0] < 0.0 or cardata[1] < 0.0 or cardata[0] > 100.0 or cardata[1] > 64.0:
+            print(BColors.WARNING + str(
+                datetime.now()) + " [WARNING] " + dronename +
+                  ": Current heading will hit or exceed boundary edge! Recalculating..." + BColors.ENDC)
+            vector = genrandomvector()
+            cardata = updatepos(vector, True, temp_data)
 
-        hexAngle = genSignal(carData[2])
+        hexangle = gensignal(cardata[2])
 
         counter = counter + 1
-        curTime = datetime.now()
-        printf(bcolors.OKBLUE + "%10s [CONSOLE]%7.5d%10s%45s%15.10f%15.10f%12.5f%12s%11.5f%10s\n" + bcolors.ENDC,
-               str(curTime), counter, droneName,
-               vector.__str__(), carData[0], carData[1], carData[2],
-               hexAngle, carData[3], droneName)
+        curtime = datetime.now()
+        printf(BColors.OKBLUE + "%10s [CONSOLE]%7.5d%10s%45s%15.10f%15.10f%12.5f%12s%11.5f%10s\n" + BColors.ENDC,
+               str(curtime), counter, dronename,
+               vector.__str__(), cardata[0], cardata[1], cardata[2],
+               hexangle, cardata[3], dronename)
 
-        socketTx(str(curTime) + "     " + hexAngle)
+        sockettx(str(curtime) + "     " + hexangle, A_SERVER_IP, A_SERVER_PORT)
 
         time.sleep(A_UPDATE_INTERVAL)
 
 
-def genRandomVector():
-    newVector = [random.uniform(-A_MAXVELOCITY, A_MAXVELOCITY), random.uniform(-A_MAXVELOCITY, A_MAXVELOCITY)]
-    return newVector
+def genrandomvector():
+    """
+
+    @return:
+    """
+
+    newvector = [random.uniform(-A_MAXVELOCITY, A_MAXVELOCITY), random.uniform(-A_MAXVELOCITY, A_MAXVELOCITY)]
+    return newvector
 
 
-def genTargetedVector():
-    """Create vector that is pointing drone towards target"""
+def gentargetedvector():
+    """
+
+    @return:
+    """
 
 
-def updatePos(vector, flag, data):
+def updatepos(vector, flag, data):
     # TODO: Calculate current location in reference to new target location
-    """Calculate new x and y coordinate based on last position"""
-    xDelta = (vector[0] * A_UPDATE_INTERVAL) + ((1 / 2) * A_ACCELERATION * (A_UPDATE_INTERVAL ** 2))
-    yDelta = (vector[1] * A_UPDATE_INTERVAL) + ((1 / 2) * A_ACCELERATION * (A_UPDATE_INTERVAL ** 2))
+    """
 
-    newData = data[:]
+    @param vector:
+    @param flag:
+    @param data:
+    @return:
+    """
 
-    newData[0] = newData[0] + xDelta  # xpos
-    newData[1] = newData[1] + yDelta  # ypos
+    xdelta = (vector[0] * A_UPDATE_INTERVAL) + ((1 / 2) * A_ACCELERATION * (A_UPDATE_INTERVAL ** 2))
+    ydelta = (vector[1] * A_UPDATE_INTERVAL) + ((1 / 2) * A_ACCELERATION * (A_UPDATE_INTERVAL ** 2))
+
+    newdata = data[:]
+
+    newdata[0] = newdata[0] + xdelta  # xpos
+    newdata[1] = newdata[1] + ydelta  # ypos
 
     if flag:
-        if yDelta >= 0:
-            newData[2] = math.atan(xDelta / yDelta) * 180 / math.pi  # angle
+        if ydelta >= 0:
+            newdata[2] = math.atan(xdelta / ydelta) * 180 / math.pi  # angle
         else:
-            newData[2] = (math.atan(xDelta / yDelta) * 180 / math.pi) - 180
+            newdata[2] = (math.atan(xdelta / ydelta) * 180 / math.pi) - 180
         if vector[1] >= 0:
-            newData[3] = math.atan(vector[1] / vector[0]) * 180 / math.pi  # heading
+            newdata[3] = math.atan(vector[1] / vector[0]) * 180 / math.pi  # heading
         else:
-            newData[3] = (math.atan(vector[1] / vector[0]) * 180 / math.pi) - 180
+            newdata[3] = (math.atan(vector[1] / vector[0]) * 180 / math.pi) - 180
     else:
-        newData[2] = 0.00
+        newdata[2] = 0.00
 
-    if newData[2] < -180:
-        newData[2] = newData[2] + 360
-    elif newData[2] > 180:
-        newData[2] = newData[2] - 360
+    if newdata[2] < -180:
+        newdata[2] = newdata[2] + 360
+    elif newdata[2] > 180:
+        newdata[2] = newdata[2] - 360
 
-    if newData[3] < 0:
-        newData[3] = newData[3] + 360
-    elif newData[3] > 360:
-        newData[3] = newData[3] - 360
+    if newdata[3] < 0:
+        newdata[3] = newdata[3] + 360
+    elif newdata[3] > 360:
+        newdata[3] = newdata[3] - 360
 
-    return newData
-
-
-def printf(format, *args):
-    sys.stdout.write(format % args)
+    return newdata
 
 
-def getGPSCoords():
-    """Poll and obtain data from GPS unit on vehicle"""
+def printf(layout, *args):
+    """
+
+    @param layout:
+    @param args:
+    @return:
+    """
+
+    sys.stdout.write(layout % args)
 
 
-def genSignal(angleValue):
-    """Craft signal based on new coordinates"""
+def getgpscoords():
+    """
+
+    @return:
+    """
+
+
+def gensignal(anglevalue):
+    """
+
+    @param anglevalue:
+    @return:
+    """
 
     # TODO: Determine what signals are needed to direct car SERVO
 
-    return float_to_hex(angleValue)
+    return float_to_hex(anglevalue)
 
     # TODO: Determine what signals are needed to direct car ESC
 
 
-def txSignal():
-    """Poll and transmit across wifi something something darkside..."""
+def txsignal():
+    """
+
+    @return:
+    """
 
     # TODO: Transmit signals to car through WiFi
 
 
 def float_to_hex(f):  # IEEE 32-bit standard for float representation
+    """
+
+    @param f:
+    @return:
+    """
+
     return hex(struct.unpack('<I', struct.pack('<f', f))[0])
 
 
-def socketTx(data):
-    # Settings
-    SERVER = "192.168.0.118"
-    PORT = 7777
+def sockettx(data, server, port):
+    """
 
+    @param data:
+    @param server:
+    @param port:
+    @return:
+    """
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        sock.connect((SERVER, PORT))
+        sock.connect((server, port))
         sock.sendall(data.encode())
-        print(bcolors.OKGREEN + "Data Sent Successfully..." + bcolors.ENDC)
+        print(BColors.OKGREEN + "Data Sent Successfully..." + BColors.ENDC)
     except ConnectionRefusedError:
-        print(bcolors.FAIL + "Connection refused...." + bcolors.ENDC)
+        print(BColors.FAIL + "Connection refused...." + BColors.ENDC)
     except TimeoutError:
-        print(bcolors.FAIL + "Connection timed out...." + bcolors.ENDC)
+        print(BColors.FAIL + "Connection timed out...." + BColors.ENDC)
 
-class bcolors:
+
+class BColors:
+    """
+
+    """
+
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -174,6 +233,12 @@ class bcolors:
 
 
 def disable(self):
+    """
+
+    @param self:
+    @return:
+    """
+
     self.HEADER = ''
     self.OKBLUE = ''
     self.OKGREEN = ''
