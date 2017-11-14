e7af00d5df8b2715a545727a95ffeaae3a239ad4
Andrew Stilwell <stilwea1@my.erau.edu>
2017-11-14 15:18:09 -0500

    Successful Raspberry Pi Implementation
    Signed-off-by: AStilwell <stilwea1@my.erau.edu>

--- Sandbox/TestReceiver.py
+++ Sandbox/TestReceiver.py
@@ -1,12 +1,18 @@
 import socket
 import sys
+import time
 
 import serial
 import serial.tools.list_ports
+import maestro
 
 HOST = ''
 PORT = 7777
 
+COM_PORT = ''
+MAX_LEFT = 4000
+MAX_RIGHT = 8000
+CENTER = 6000
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,29 +27,58 @@ def main():
     print("SERIAL TESTING...Please Wait.")
 
     print(serial_debug())
+    test_controller(COM_PORT)
 
     print("TESTING COMPLETE...")
 
     print("SERVER ESTABLISHED...")
 
-    while True:
-        (conn, address) = sock.accept()
-        data = conn.recv(64)
+    #while True:
+    #    (conn, address) = sock.accept()
+    #    data = conn.recv(64)
 
-        try:
-            print("Received data from: " + conn.getpeername().__str__() + '\t\t' + data.__str__())
-        except TypeError as emsg2:
-            print(emsg2)
-            sys.exit()
+    #    try:
+    #        print("Received data from: " + conn.getpeername().__str__() + '\t\t' + data.__str__())
+    #    except TypeError as emsg2:
+    #        print(emsg2)
+    #        sys.exit()
 
 
 def serial_debug():
+    global COM_PORT
+    
     list = serial.tools.list_ports.comports()
     available = []
     for port in list:
         available.append(port.device)
+    
+    COM_PORT = available[1]
 
     return available
 
 
+def test_controller(port):
+    servo = maestro.Controller(port)
+    print('SERVO CONNECTION')
+
+    # 3 ESC, 5 STEERING
+
+    servo.setAccel(5, 0)
+    servo.setAccel(3, 0)
+    print(servo.getMin(5), servo.getMax(5))
+
+    print('SENT SIGNAL')
+    
+    servo.setTarget(5, MAX_RIGHT)
+    print(servo.getPosition(5))
+    time.sleep(1)
+    servo.setTarget(5, MAX_LEFT)
+    print(servo.getPosition(5))
+    time.sleep(1)
+    servo.setTarget(5, MAX_RIGHT)
+    print(servo.getPosition(5))
+    time.sleep(1)
+    servo.setTarget(5, CENTER)
+    
+    servo.close()
 main()
