0dedf19d10b6ee901198c0bd3db595790634d0cd
Andrew Stilwell <stilwell.andrewk@gmail.com>
2018-04-12 18:19:39 +0000

    Updated script

--- Client/autostart.sh
+++ Client/autostart.sh
@@ -4,7 +4,7 @@ echo '*****************************************'
 echo '*********** Python Ver. Check ***********'
 echo '*****************************************'
 
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python3.6|grep "install ok installed")
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python3.5|grep "install ok installed")
 echo Checking for python3.5: $PKG_OK
 if [ "" == "$PKG_OK" ]; then
   echo "Missing python3.5. Setting up python3.5 and pip."
@@ -16,7 +16,7 @@ echo '*****************************************'
 echo '************ Git Repo Update ************'
 echo '*****************************************'
 
-cd /git/FriendorFoe
+cd ~/git/FriendorFoe
 
 git pull
 
